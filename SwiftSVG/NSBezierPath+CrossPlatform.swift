1e5978d005287dfd030badc95df1af44c6267eb5
Alexandr Esilevich <aeh@scade.io>
2024-01-17 17:11:30 +0000

    added default switch case into NSBezierPath.cgPath implementation (fix for building with Xcode 15)

--- SwiftSVG/NSBezierPath+CrossPlatform.swift
+++ SwiftSVG/NSBezierPath+CrossPlatform.swift
@@ -48,6 +48,8 @@ public extension NSBezierPath {
                     path.addCurve(to: points[2], control1: points[0], control2: points[1])
                 case .closePath:
                     path.closeSubpath()
+                default:
+                    break
                 }
             }
             points.deallocate()
